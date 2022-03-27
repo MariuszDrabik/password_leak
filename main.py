52f698a84bf4ba92d6856f8ca7fe04e3bf59c812
Mariusz Drabik <drabik.mariusz@gmail.com>
2022-03-27 18:33:06 +0200

    first test

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print('Hello Word')
+from requests import get
+import hashlib
+
+password_to_hash = "Mariusz1"
+
+passwd_hashed = hashlib.sha1(password_to_hash.encode())
+sample = 'e01dc0a4ab8236bdaa835dda82dd1d0b401b65bc'
+
+print("The hexadecimal equivalent of SHA-1 is : ")
+print(passwd_hashed.hexdigest())
+print(sample)
+api = 'https://api.pwnedpasswords.com/range/'
+# request = get(api)
+# print(request.status_code)
+
+
